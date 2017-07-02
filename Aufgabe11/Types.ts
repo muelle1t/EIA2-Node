3a7a91de06843c4f9e86bdb24eb6da41f117f9e1
annka <annka@DESKTOP-Q0CC7MP.SpeedportEntry2090126200030>
2017-07-02 17:24:59 +0200

    Aufgabe 11

---
+++ Aufgabe11/Types.ts
@@ -0,0 +1,13 @@
+/*
+Aufgabe: Aufgabe 11
+Name: Annkathrin Müller
+Matrikelnr.: 254868
+Datum: 02.07.2017
+
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. 
+Er wurde nicht kopiert und auch nicht diktiert. /**/
+
+
+
+
+
